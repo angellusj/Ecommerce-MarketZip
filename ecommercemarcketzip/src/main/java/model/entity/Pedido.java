600cb4726c5940053b0fcab207fcc11fe68b60b4
Everson-Alisson <eversonalisson365@gmail.com>
2025-11-26 19:28:28 -0300

    feat: adicionando construtores, getters e setters e toString de pedido

--- ecommercemarcketzip/src/main/java/model/entity/Pedido.java
+++ ecommercemarcketzip/src/main/java/model/entity/Pedido.java
@@ -1,5 +1,55 @@
 package model.entity;
+import java.sql.Date;
 
 public class Pedido {
+    private int idPedido;
+    private Date data;
+    private Boolean finalizar;
+    private double valorTotal;
     
+public Pedido(int idPedido, Date data, Boolean finalizar, double valorTotal) {
+        this.idPedido = idPedido;
+        this.data = data;
+        this.finalizar = finalizar;
+        this.valorTotal = valorTotal;
+ }
+
+
+@Override
+public String toString() {
+    return "Pedido idPedido=" + idPedido + ", data=" + data + ", finalizar=" + finalizar + ", valorTotal=" + valorTotal;
+}
+
+
+public int getIdPedido() {
+    return idPedido;
+}
+
+public void setIdPedido(int idPedido) {
+    this.idPedido = idPedido;
+}
+
+public Date getData() {
+    return data;
+}
+
+public void setData(Date data) {
+    this.data = data;
+}
+
+public Boolean getFinalizar() {
+    return finalizar;
+}
+
+public void setFinalizar(Boolean finalizar) {
+    this.finalizar = finalizar;
+}
+
+public double getValorTotal() {
+    return valorTotal;
+}
+
+public void setValorTotal(double valorTotal) {
+    this.valorTotal = valorTotal;
+} 
 }
