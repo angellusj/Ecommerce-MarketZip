ca9e78a1b2946cdf0c3f0010ea18b4906ad5bf71
Artur <arthur.gama.ag.12@gmail.com>
2025-11-26 19:03:45 -0300

    ✨ feat: classe funcionario adicionada

---
+++ ecommercemarcketzip/src/main/java/model/entity/Funcionario.java
@@ -0,0 +1,26 @@
+package model.entity;
+
+public class Funcionario extends Usuario {
+    private String cargo;
+
+    public Funcionario(int idUsuario, String nome, String cpf, String email, String telefone, String senha, String cargo){
+        super(idUsuario, nome, cpf, email, telefone, senha);
+        this.cargo = cargo;
+        
+    }
+
+    public void setCargo(String cargo) {
+        this.cargo = cargo;
+    }
+
+    public String getCargo() {
+        return cargo;
+    }
+
+    @Override
+    public String toString() {
+        return super.toString() + "cargo: " + cargo;
+    }
+
+    
+}
